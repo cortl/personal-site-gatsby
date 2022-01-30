d142bf7151be2c9d99cef06b30df313b34125cdb
Cortlan Bainbridge <cortlan.bainbridge@gmail.com>
2022-01-30 14:13:12 -0600

    fixing this by upgrading packages + removing unused package that was leftover from when I was worse at react lol

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sharp`,
-    `gatsby-transformer-json`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
